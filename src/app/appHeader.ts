f352d069d103e73d4460bc48491b823bb1b8d38d
Subhajit Dinda <subhajit.dinda@gmail.com>
2025-09-12 11:56:02 +0530

    string interpolation for property binding

--- src/app/appHeader.ts
+++ src/app/appHeader.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-header',
     templateUrl: './appHeader.html',
-    styleUrl: './appheader.css'
+    styleUrl: './appHeader.css'
 })
 export class AppHeader {
+
+    helpImageLocation="assets/32210603-help-icon.jpg"
 
 }
